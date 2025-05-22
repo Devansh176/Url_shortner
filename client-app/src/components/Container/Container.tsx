c2c871e626ada2756f4a39bcd934d8daf0a32fb7
Devansh <devanshdhopte@gmail.com>
2025-05-22 19:36:09 +0530

    API connection sucessfully established

--- client-app/src/components/Container/Container.tsx
+++ client-app/src/components/Container/Container.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import FormContainer from '../FormContainer/FormContainer';
+import type { UrlData } from '../../interface/urlData';
+import { serverUrl } from '../../helpers/Constants';
+import axios from 'axios';
+import DataTable from '../DataTable/DataTable';
 
-interface IContainerProps {
-}
+interface IContainerProps {}
 
 const Container: React.FunctionComponent<IContainerProps> = () => {
+  const [data, setData] = React.useState<UrlData[]>([]);
+
+  const fetchTableData = async () => {
+    const response = await axios.get(`${serverUrl}/shortUrl`);
+    console.log("The response from the server is : ", response);
+    setData(response.data);
+  };
+
+  React.useEffect(() => {
+    fetchTableData();
+  }, []);
+
+
   return <>
     <FormContainer/>
+    <DataTable data={data}/>
   </>
 };
 
